9c6a70cfe20215ba0a3ebbf47c6386ba78a13da7
Noah Lambe <noahalambe@gmail.com>
2025-06-15 12:33:25 -0230

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/test/java/com/keyin/airportapi/AirportapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.airportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
