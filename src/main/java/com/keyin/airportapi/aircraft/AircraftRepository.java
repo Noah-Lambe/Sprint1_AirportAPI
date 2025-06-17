67ddb7010f7631782f668a6f537339dc7289a1a8
Jonathan <jonathan.strickland2012@gmail.com>
2025-06-17 12:40:09 -0230

    Added Aircraft Repository

---
+++ src/main/java/com/keyin/airportapi/aircraft/AircraftRepository.java
@@ -0,0 +1,6 @@
+package com.keyin.airportapi.aircraft;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+}
