ad5159c4f4c0853e6396200751a7ecad9a697b04
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-19 12:50:38 -0230

    Airport Controller methods added, added crud to AirportRepository

--- src/main/java/com/keyin/airportapi/airport/AirportRepository.java
+++ src/main/java/com/keyin/airportapi/airport/AirportRepository.java
@@ -1,4 +1,7 @@
 package com.keyin.airportapi.airport;
 
-public interface AirportRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface AirportRepository extends CrudRepository<Airport, Integer> {
+
 }
