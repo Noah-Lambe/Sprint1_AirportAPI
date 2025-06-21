1b9667aeaf3647e5730ee2e263c2806355a350fa
KeyinhunterS <Hunter.Saunders@keyin.com>
2025-06-21 15:46:13 -0230

    Add getters and setters to Passenger entity

--- src/main/java/com/keyin/airportapi/passenger/Passenger.java
+++ src/main/java/com/keyin/airportapi/passenger/Passenger.java
@@ -30,4 +30,51 @@ public class Passenger {
     private Set<Aircraft> aircraft = new HashSet<>();
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Set<Aircraft> getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(Set<Aircraft> aircraft) {
+        this.aircraft = aircraft;
+    }
 }
