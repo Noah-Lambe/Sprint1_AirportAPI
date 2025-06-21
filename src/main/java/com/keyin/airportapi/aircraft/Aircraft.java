05f91f8751839dfebef219f5f8ec60a3c4204aed
Jonathan <jonathan.strickland2012@gmail.com>
2025-06-21 14:15:55 -0230

    Added Aircraft

--- src/main/java/com/keyin/airportapi/aircraft/Aircraft.java
+++ src/main/java/com/keyin/airportapi/aircraft/Aircraft.java
@@ -8,9 +8,9 @@ public class Aircraft {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long aircraftId;
 
-    private String type;
+    private String name;
     private String airlineName;
     private int numberOfPassengers;
 
@@ -27,26 +27,57 @@ public class Aircraft {
 
     public Aircraft() {}
 
-    public Aircraft(String type, String airlineName, int numberOfPassengers) {
-        this.type = type;
+    public Aircraft(String name, String airlineName, int numberOfPassengers) {
+        this.name = name;
         this.airlineName = airlineName;
         this.numberOfPassengers = numberOfPassengers;
     }
 
-    public Long getId() { return id; }
+    public Long getAircraftId() {
+        return aircraftId;
+    }
 
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
+    public void setAircraftId(Long aircraftId) {
+        this.aircraftId = aircraftId;
+    }
 
-    public String getAirlineName() { return airlineName; }
-    public void setAirlineName(String airlineName) { this.airlineName = airlineName; }
+    public String getName() {
+        return name;
+    }
 
-    public int getNumberOfPassengers() { return numberOfPassengers; }
-    public void setNumberOfPassengers(int numberOfPassengers) { this.numberOfPassengers = numberOfPassengers; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public List<Passenger> getPassengers() { return passengers; }
-    public void setPassengers(List<Passenger> passengers) { this.passengers = passengers; }
+    public String getAirlineName() {
+        return airlineName;
+    }
 
-    public List<Airport> getAirports() { return airports; }
-    public void setAirports(List<Airport> airports) { this.airports = airports; }
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
 }
