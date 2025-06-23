4f7b11e6d26c055ae7d16b5fa6a609f654ea6a3b
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-22 21:39:13 -0230

    Added city support to all airport related classes

--- src/test/java/com/keyin/airportapi/airport/tests/AirportRepositoryTest.java
+++ src/test/java/com/keyin/airportapi/airport/tests/AirportRepositoryTest.java
@@ -19,14 +19,23 @@ public class AirportRepositoryTest {
     @Autowired
     private AirportRepository airportRepository;
 
+    @Autowired
+    private CityRepository cityRepository;
+
     private Airport airport;
 
     @BeforeEach
     public void setUp() {
+        testCity = new City();
+        testCity.setName("Testville");
+        testCity.setState("Testland");
+        testCity.setPopulation(500000);
+        testCity = cityRepository.save(testCity);
+
         airport = new Airport();
-        airport.setAirportId(1);
         airport.setAirportName("Test International Airport");
         airport.setAreaCode("TIA");
+        airport.setCity(testCity);
     }
 
     @Test
@@ -34,20 +43,25 @@ public class AirportRepositoryTest {
         Airport savedAirport = airportRepository.save(airport);
         assertThat(savedAirport).isNotNull();
         assertThat(savedAirport.getAirportName()).isEqualTo("Test International Airport");
+        assertThat(savedAirport.getCity()).isNotNull();
+        assertThat(savedAirport.getCity().getName()).isEqualTo("Testville");
     }
 
     @Test
     public void testFindById() {
         Airport savedAirport = airportRepository.save(airport);
         Optional<Airport> retrieved = airportRepository.findById(savedAirport.getAirportId());
+
         assertThat(retrieved).isPresent();
         assertThat(retrieved.get().getAreaCode()).isEqualTo("TIA");
+        assertThat(retrieved.get().getCity().getName()).isEqualTo("Testville");
     }
 
     @Test
     public void testDeleteAirport() {
         Airport savedAirport = airportRepository.save(airport);
         airportRepository.deleteById(savedAirport.getAirportId());
+
         Optional<Airport> result = airportRepository.findById(savedAirport.getAirportId());
         assertThat(result).isNotPresent();
     }
@@ -56,10 +70,16 @@ public class AirportRepositoryTest {
     public void testFindAllAirports() {
         airportRepository.save(airport);
 
+        City secondCity = new City();
+        secondCity.setName("Second City");
+        secondCity.setState("ProvinceX");
+        secondCity.setPopulation(300000);
+        secondCity = cityRepository.save(secondCity);
+
         Airport airport2 = new Airport();
-        airport2.setAirportId(2);
         airport2.setAirportName("Second Airport");
         airport2.setAreaCode("SAP");
+        airport2.setCity(secondCity);
 
         airportRepository.save(airport2);
 
