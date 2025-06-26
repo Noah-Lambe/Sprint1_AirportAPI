a1af3e55d46d040750ad4e15c8b0e56a5e2d49ac
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-26 16:22:07 -0230

    added getmapping to retreive airports per aircraft

--- src/main/java/com/keyin/airportapi/aircraft/AircraftController.java
+++ src/main/java/com/keyin/airportapi/aircraft/AircraftController.java
@@ -1,5 +1,6 @@
 package com.keyin.airportapi.aircraft;
 
+import com.keyin.airportapi.airport.Airport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,16 @@ public class AircraftController {
         return aircraftService.getAircraftById(id)
         .map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Airport>> getAirportsUsedByAircraft(@PathVariable("id") Long aircraftId) {
+        try {
+            List<Airport> airports = aircraftService.getAirportsUsedByAircraft(aircraftId);
+            return ResponseEntity.ok(airports);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
 
     @PostMapping
