e736db27c9abbfb0ff4ae866720e19e188985f65
Noah <Noah.Whiffen@keyin.com>
2025-08-09 15:39:45 -0230

    DataLoader class started with constants and constructor, starting run method

---
+++ src/main/java/com/keyin/airportapi/loader/DataLoader.java
@@ -0,0 +1,59 @@
+package com.keyin.airportapi.loader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keyin.airportapi.aircraft.Aircraft;
+import com.keyin.airportapi.aircraft.AircraftRepository;
+import com.keyin.airportapi.airline.AirlineRepository;
+import com.keyin.airportapi.airport.AirportRepository;
+import com.keyin.airportapi.city.CityRepository;
+import com.keyin.airportapi.flight.FlightRepository;
+import com.keyin.airportapi.gate.GateRepository;
+import com.keyin.airportapi.passenger.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired private final AircraftRepository aircraftRepository;
+    @Autowired private final AirlineRepository airlineRepository;
+    @Autowired private final AirportRepository airportRepository;
+    @Autowired private final CityRepository cityRepository;
+    @Autowired private final FlightRepository flightRepository;
+    @Autowired private final GateRepository gateRepository;
+    @Autowired private final PassengerRepository passengerRepository;
+
+    public DataLoader(AircraftRepository aircraftRepository,
+                      AirlineRepository airlineRepository,
+                      AirportRepository airportRepository,
+                      CityRepository cityRepository,
+                      FlightRepository flightRepository,
+                      GateRepository gateRepository,
+                      PassengerRepository passengerRepository) {
+        this.aircraftRepository = aircraftRepository;
+        this.airlineRepository = airlineRepository;
+        this.airportRepository = airportRepository;
+        this.cityRepository = cityRepository;
+        this.flightRepository = flightRepository;
+        this.gateRepository = gateRepository;
+        this.passengerRepository = passengerRepository;
+    }
+
+    @Override
+    public void run(String... args)  throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream aircraftStream = getClass().getResourceAsStream("/data.json");
+        List<Aircraft> aircrafts = Arrays.asList(mapper.readValue(aircraftStream, Aircraft[].class));
+        aircraftRepository.saveAll(aircrafts);
+
+        InputStream airlineStream = getClass()
+    }
+
+
+}
